cd02e5753038943d72380e46f02a3d8a82385511
AndreaMtzR <119258769+AndreaMtzR@users.noreply.github.com>
2022-11-27 19:19:35 -0600

    Add files via upload

---
+++ src/main/java/com/example/gps/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.gps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+    EditText txtLatitud,txtLongitud;
+    GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); txtLatitud= findViewById(R.id.txtLatitud);
+        txtLongitud= findViewById(R.id.txtLongitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap= googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng mexico = new LatLng(20.568201065063477,-99.90568542480469);
+        mMap.addMarker(new MarkerOptions().position(mexico).title("Negocio"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
+
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+
+        mMap.clear();
+        LatLng mexico = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(mexico).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+        mMap.clear();
+        LatLng mexico = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(mexico).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
+
+    }
+}
